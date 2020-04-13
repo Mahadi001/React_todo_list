91da7c624b25e768fb9dff6f4f9a1f138fee3cad
Mohammed Abu-Al Mahadi <mohammed.abumahadi@northsouth.edu>
2020-04-13 19:57:30 +0600

    line_through

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TodoForm from "./TodoForm";
-
+import Todo from "./Todo";
 export default class TodoList extends React.Component {
   state = {
     todos: [],
@@ -12,13 +12,32 @@ export default class TodoList extends React.Component {
     });
   };
 
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          };
+        } else {
+          return todo;
+        }
+      }),
+    });
+  };
+
   render() {
     return (
       <div>
         todos...
         <TodoForm onSubmit={this.addTodo} />
         {this.state.todos.map((todo) => (
-          <div key={todo.id}>{todo.text}</div>
+          <Todo
+            key={todo.id}
+            togggleComlplete={() => this.toggleComplete(todo.id)}
+            todo={todo}
+          />
         ))}
       </div>
     );
